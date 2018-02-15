bf06882457c8a664035ca9224dff16d66356b344
Florian ROMEO <florian.romeo@feragon.net>
2018-02-15 22:48:06 +0100

    Ajout déplacement toucher

--- projet/core/src/net/feragon/pacman/input/PacmanInputProcessor.java
+++ projet/core/src/net/feragon/pacman/input/PacmanInputProcessor.java
@@ -1,5 +1,6 @@
 package net.feragon.pacman.input;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 
 import net.feragon.pacman.model.Pacman;
@@ -33,8 +34,11 @@ public class PacmanInputProcessor implements InputProcessor {
 			case 22:
 				pacman.setPosition(pacman.getPosition().add(1, 0));
 				break;
+				
+			default:
+				return false;
 		}
-		return false;
+		return true;
 	}
 
 	@Override
@@ -51,8 +55,28 @@ public class PacmanInputProcessor implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
+		float halfWidth = Gdx.graphics.getWidth() / 2;
+		float halfHeight = Gdx.graphics.getHeight() / 2;
+		screenX -= halfWidth;
+		screenY -= halfHeight;
+		
+		System.out.println(screenX);
+		System.out.println(screenY);
+		
+		if(screenY > Math.abs(screenX)) {
+			pacman.setPosition(pacman.getPosition().add(0, -1));
+		}
+		else if(screenY < -Math.abs(screenX)) {
+			pacman.setPosition(pacman.getPosition().add(0, 1));
+		}
+		else if(screenX < Math.abs(screenY)) {
+			pacman.setPosition(pacman.getPosition().add(-1, 0));
+		}
+		else {
+			pacman.setPosition(pacman.getPosition().add(1, 0));
+		}
+		
+		return true;
 	}
 
 	@Override
