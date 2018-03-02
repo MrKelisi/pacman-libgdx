0ab796dce7b2bcd3ae8c67fdc3c1d96eb939b57c
Florian Roméo <florian.romeo@feragon.net>
2018-03-02 10:37:08 +0100

    Déplacement monstre rose

--- projet/core/src/net/feragon/pacman/model/PinkMonster.java
+++ projet/core/src/net/feragon/pacman/model/PinkMonster.java
@@ -1,5 +1,7 @@
 package net.feragon.pacman.model;
 
+import java.util.EnumSet;
+
 import com.badlogic.gdx.math.Vector2;
 
 public class PinkMonster extends Monster {
@@ -9,7 +11,23 @@ public class PinkMonster extends Monster {
 	
 	@Override
 	protected Direction getNewDirection() {
-		// TODO Auto-generated method stub
-		return Direction.LEFT;
+		EnumSet<Direction> directions = getPossibleDirections();
+		
+		Vector2 pacman = world.getMaze().getPacman().getPosition();
+		if(pacman.x < getPosition().x && directions.contains(Direction.LEFT)) {
+			return Direction.LEFT;
+		}
+		else if(pacman.x > getPosition().x && directions.contains(Direction.RIGHT)) {
+			return Direction.RIGHT;
+		}
+		else if(pacman.y > getPosition().y && directions.contains(Direction.UP)) {
+			return Direction.UP;
+		}
+		else if(pacman.y < getPosition().y && directions.contains(Direction.DOWN)) {
+			return Direction.DOWN;
+		}
+		else {
+			return (Direction) directions.toArray()[0];
+		}
 	}
 }
